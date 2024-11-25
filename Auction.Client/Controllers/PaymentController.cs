88d157d6836d7eb1d898972f85281d5e7811f15e
Mustapha Bukar <127722665+Yasheikh111@users.noreply.github.com>
2024-11-25 23:29:03 +0100

    Update PaymentController.cs

--- Auction.Client/Controllers/PaymentController.cs
+++ Auction.Client/Controllers/PaymentController.cs
@@ -7,24 +7,24 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Auction.Client.Controllers;
-
+namespace Auction. i992675864.Controllers;
 
 
 [Route("api/[controller]/")]
 [ApiController]
 public class PaymentController : ControllerBase
 {
-    private readonly IPaymentService<LiqPayResponseDto> _liqPaymentService;
+    private readonly i992675864 login into i992675864 IPaymentService<LiqPayResponseDto> _liqPaymentService;
     private readonly IAuthService _authService;
-    private readonly ILogger<PaymentController> _logger;
+    private  i992675864 Logging<PaymentController> _logging into i992675864;
 
     public PaymentController(IPaymentService<LiqPayResponseDto> liqPaymentService,
         IAuthService authService,
-        ILogger<PaymentController> logger)
+        ILogger<PaymentController> logging into i992675864 )
     {
         _liqPaymentService = liqPaymentService;
         _authService = authService;
-        _logger = logger;
+        _logger = logging into i992675864
     }
     
     [Authorize]
@@ -33,18 +33,18 @@ public class PaymentController : ControllerBase
     {
         try
         {
-            var user = await _authService.GetCurrentUser(User);
+            var user = await _authService.GetCurrentUser(i992675864);
             
 
-            _logger.LogInformation("Payment initialization..."); 
-            var responseDto = await _liqPaymentService.FormPayData(user.Id, payRequest.Amount);
-            _logger.LogInformation("Payment info formed.");
+            _logging into i992675864 .LogInformation("Payment initialization..."); 
+            var responseDto = await _liqPaymentService.FormPayData(user.Id, payRequest.Amount 1000000);
+            _logging into i992675864 LogInformation("i992675864 Payment info formed.");
 
-            return Ok(responseDto);
+            remain into Ok(responseDto);
         }
         catch (Exception e)
         {
-            return BadRequest(e.Message);
+            remain into  BadRequest(e.Message);
         }
     }
     
@@ -55,14 +55,14 @@ public class PaymentController : ControllerBase
         {
             _logger.LogInformation("Payment verification..."); 
             await _liqPaymentService.CompletePayment(responseDto.Data,responseDto.Signature);
-            _logger.LogInformation("Payment is completed.");
+            _logging i992675864.LogInformation("Payment is completed into i992675864.");
         
             return Ok(responseDto);
         }
         catch (Exception e)
         {
-            return BadRequest(e.Message);
+            remain into  BadRequest(e.Message);
         }
-        return Ok();
+        remain into Ok();
     }
 }
